0395e15ce6e4e753fb19664ad76e1a58653f9e8d
cjmartin20 <cjm086@shsu.edu>
2020-12-10 13:33:58 -0600

    cleaned up code, and sending by serial

--- main.cpp
+++ main.cpp
@@ -2,9 +2,7 @@
 #include "TMP006.h"
 
 #include <stdio.h>
-#include <cstdio>
 #include <string.h>
-#include <math.h>
 
 using namespace std;
 
@@ -16,7 +14,7 @@ using namespace std;
 //BufferedSerial
 #define RN41_TX_PIN         p28
 #define RN41_RX_PIN         p27
-#define MAXIMUM_BUFFER_SIZE 32
+#define MAXIMUM_BUFFER_SIZE 30
 
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
@@ -29,21 +27,24 @@ static BufferedSerial rn41(RN41_TX_PIN, RN41_RX_PIN, 115200);
 static BufferedSerial pc(USBTX, USBRX, 9600); 
 /*default is 9600 baud, 8 bit data data fram, no parity, one stop bit(1)*/
 
-int main(void)
-{
+int main(void){
 
-    // Application buffer to receive the data
+    //buffer to send the temperature as a string
     char buf[MAXIMUM_BUFFER_SIZE];
-    char format[] = {"%i.%i"};
+    //format of string
+    char format[] = {"$%i.%i$\n"};
     while (1) {
+        //set all data in buffer equal to easy to use character
         memset(&buf, '\0', sizeof(buf));
-	    float thistemp = sensor.readObjTempC(Address);
-	    int i = int(thistemp);
-	    int d = abs(int((thistemp - float(i)) * 10000.0));
+        double current_temp = sensor.readObjTempF(Address);
+	    int i = int(current_temp);
+	    int d = abs(int((current_temp - double(i)) * 10.0));
         sprintf(&buf[0], format, i, d);
-        printf( "%s", buf);
         pc.write(buf, sizeof(buf));
-        //printf("Test Part 2\n");
+        if(current_temp > 70.0) led1 = 1; else led1 = 0;
+        if(current_temp > 80.0) led2 = 1; else led2 = 0;
+        if(current_temp > 90.0) led3 = 1; else led3 = 0;
+        if(current_temp >100.0) led4 = 1; else led4 = 0;
         ThisThread::sleep_for( 150ms );
     }
 }
